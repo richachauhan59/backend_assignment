506d2b513ace388b72cd62e658bc03db7d7d8931
richachauhan59 <richachauhan2019@gmail.com>
2020-12-18 18:33:22 +0530

    Added Email Validation and user delete functionality

--- routes/user.js
+++ routes/user.js
@@ -10,31 +10,57 @@
   const User = require('../models/user')
 
   router.post("/signup", (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if(err){
-            return res.status(500).json({
-                error: err
+      User.find({email:req.body.email})
+      .exec()
+      .then(user => {
+          if(user >= 1){
+            return res.status(409).json({
+                message:"Email Already Exists"
             })
-        }
-        else{
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password : hash
-            })
-            user.save()
-            .then(result => {
-                res.status(201).json({
-                    message:'User Created'
-                })
-            }).catch(err => {
-                console.log(err)
-                res.status(500).json({
-                    error:err
-                })
-            })
-        }
-      
+          }
+          else{
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if(err){
+                    return res.status(500).json({
+                        error: err
+                    })
+                }
+                else{
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password : hash
+                    })
+                    user.save()
+                    .then(result => {
+                        res.status(201).json({
+                            message:'User Created'
+                        })
+                    }).catch(err => {
+                        console.log(err)
+                        res.status(500).json({
+                            error:err
+                        })
+                    })
+                }
+              
+              })
+          }
+      })
+  })
+
+  router.delete("/:userId", (req, res, next) => {
+      User.remove({_id: req.params.userId})
+      .exec()
+      .then( result => {
+          res.status(200).json({
+              message: "User Deleted"
+          })
+      } )
+      .catch(err => {
+          res.status(500).json({
+              error: err
+          })
       })
   })
 
