1441454251ce5e252d3b9988d139a29c684f213d
richachauhan59 <richachauhan2019@gmail.com>
2020-12-18 19:09:17 +0530

    User is able to login with valid id and password

--- routes/user.js
+++ routes/user.js
@@ -49,6 +49,38 @@
       })
   })
 
+  router.post('/login', (req, res, next) => {
+      User.find({email : req.body.email})
+      .exec()
+      .then(user => {
+        if(user.length < 1){
+            return res.status(404).json({
+                message: "Auth Failed"
+            })
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if(err){
+                return res.status(401).json({
+                    message: 'Auth Failed'
+                })
+            }
+            if(result){
+                return res.status(200).json({
+                    message: "User Login Successfully"
+                })
+            }
+            res.status(401).json({
+                message: 'Auth Failed'
+            })
+        })
+      })
+      .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+  })
+
   router.delete("/:userId", (req, res, next) => {
       User.remove({_id: req.params.userId})
       .exec()
